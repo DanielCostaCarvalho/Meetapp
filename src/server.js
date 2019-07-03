c2a1783e98482ea82487a3f471722d84ab02bbc3
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2019-07-03 10:14:13 -0300

    Dividindo o hello world em app, routes e server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,3 @@
-import express from "express";
-import Router from "express";
+import app from "./app";
 
-const server = express();
-const routes = Router();
-routes.get("/", (req, res) => {
-  return res.status(200).json({ hello: "world" });
-});
-
-server.use(routes);
-
-server.listen(3333);
+app.listen(3333);
